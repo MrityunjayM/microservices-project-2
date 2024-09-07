ebd7dc842f40cc7049e2559d92667d1e26db113f
MrityunjayM <68852546+MrityunjayM@users.noreply.github.com>
2024-09-08 02:25:49 +0530

    test correction

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,22 +1,16 @@
 import Stripe from "stripe";
 
-// export const stripe = new Stripe(process.env.STRIPE_KEY!, {
-//   apiVersion: "2024-04-10",
-// });
+export const stripe = new Stripe(process.env.STRIPE_KEY!, {
+  apiVersion: "2024-04-10",
+});
 
-// console.log(process.env.STRIPE_KEY);
-
-export const stripe = new Stripe(
-  "sk_test_51PD8pYSFAbrYC1aBEYZAYS0BEETgHwK4JebWc4ZAi2XJ9ou4g01eVToDOBEXMG2jIYpD9lOSpvRSKP2BJhp2ItWD00zI1qHtOy",
-  { apiVersion: "2024-04-10" }
-);
-
-async function cleanupPendingPayments() {
+export async function cleanupPendingPayments() {
   const paymentIntents = await stripe.paymentIntents.list({
     limit: 5,
   });
 
-  paymentIntents.data.forEach(({ id }) => {
-    stripe.paymentIntents.cancel(id);
-  });
+  for (const payment_intent of paymentIntents.data) {
+    if (payment_intent.status === "succeeded") continue;
+    await stripe.paymentIntents.cancel(payment_intent.id);
+  }
 }
